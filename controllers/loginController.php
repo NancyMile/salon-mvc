c0d8314fccc191fe520758ff3fa3447540cea0e7
nancy <smithnancy@outlook.com>
2023-08-14 20:49:22 +1000

    adding routing and methods for authentication

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -2,7 +2,24 @@
 namespace Controllers;
 
 class loginController{
+
     public static function login(){
         echo "Login";
+    }
+
+    public static function logout(){
+        echo "Logout";
+    }
+
+    public static function forgot(){
+        echo "forgot";
+    }
+
+    public static function recover(){
+        echo "recover";
+    }
+
+    public static function create(){
+        echo "create";
     }
 }
--- public/index.php
+++ public/index.php
@@ -9,6 +9,19 @@ $router = new Router();
 
 //init sesion
 $router->get('/',[loginController::class,'login']);
+$router->post('/',[loginController::class,'login']);
+$router->get('/logout',[loginController::class,'logout']);
+
+//recover pass
+$router->get('/forgot',[loginController::class,'forgot']);
+$router->post('/forgot',[loginController::class,'forgot']);
+$router->get('/recover',[loginController::class,'recover']);
+$router->post('/recover',[loginController::class,'recover']);
+
+//create account
+$router->get('/create-account',[loginController::class,'create']);
+$router->post('/create-account',[loginController::class,'create']);
+
 
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
