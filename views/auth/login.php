84f9e807df3b2a25e639287f9d02d20d2f96e1ee
nancy <smithnancy@outlook.com>
2023-08-17 20:36:46 +1000

    view login form created  styles added

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,2 +1,17 @@
 <h1 class="page-name">Login</h1>
 <p class="page-description"> Start Session</p>
+<form class="form" method="POST" action="/">
+    <div class="field">
+        <label for="email">Email</label>
+        <input type="email" id="email" placeholder="email" name="email">
+    </div>
+    <div class="field">
+        <label for="password">Password</label>
+        <input type="password" id="password" placeholder="password" name="password">
+    </div>
+    <input class="button" type="button" value="Start Session">
+</form>
+<div class="actions">
+    <a href="/create-account">Create Account</a>
+    <a href="/forgot">Forgot Password</a>
+</div>
