3a21951b526979162c57f5f8a0702de5addefd74
nancy <smithnancy@outlook.com>
2023-08-23 09:58:49 +1000

    create service form added

--- views/services/create.php
+++ views/services/create.php
@@ -1,3 +1,7 @@
 <h1 class="page-name">Create Service</h1>
 <p class="page-description">Create Service</p>
 <?php include_once __DIR__.'/../templates/bar.php' ?>
+<form action="/services/create" method="POST" class="form">
+    <?php include_once __DIR__.'/form.php' ?>
+    <input type="submit" value="Save Service" class="button">
+</form>
---
+++ views/services/form.php
@@ -0,0 +1,8 @@
+<div class="field">
+    <label for="name">Name:</label>
+    <input type="text" name="name" id="name" placeholder="name">
+</div>
+<div class="field">
+    <label for="price">Price:</label>
+    <input type="number" name="price" id="price">
+</div>
