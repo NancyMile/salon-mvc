baeb0838db2e1a3ab8e63440a5d64adf00c8e06a
nancy <smithnancy@outlook.com>
2023-08-23 12:55:55 +1000

    applied env

--- classes/Email.php
+++ classes/Email.php
@@ -20,11 +20,11 @@ use PHPMailer\PHPMailer\PHPMailer;
         //create object email
         $email = new PHPMailer();
         $email->isSMTP();
-        $email->Host = 'sandbox.smtp.mailtrap.io';
+        $email->Host = $_ENV['EMAIL_HOST'];
         $email->SMTPAuth = true;
-        $email->Port = 2525;
-        $email->Username = '';
-        $email->Password = '';
+        $email->Port = $_ENV['EMAIL_PORT'];
+        $email->Username = $_ENV['EMAIL_USER'];
+        $email->Password = $_ENV['EMAIL_PASSWORD'];
 
         $email->setFrom('salonAccount@test.com');
         $email->addAddress('salonAccount@tests.com','salon.com');
@@ -36,7 +36,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 
         $content = "<html>";
         $content .= "<p> Hi: ".$this->name." Please confirm by click following link</p>";
-        $content .= "<a href='http://localhost:8080/verify-account?token=".$this->token."'>Confirm Acoount</a>";
+        $content .= "<a href='".$_ENV['APP_URL']."/verify-account?token=".$this->token."'>Confirm Acoount</a>";
         $content .= "<p>If you did not create a new account, please ignore the email.</p>";
         $content .= "</html>";
         $email->Body = $content;
@@ -49,11 +49,11 @@ use PHPMailer\PHPMailer\PHPMailer;
         //create object email
         $email = new PHPMailer();
         $email->isSMTP();
-        $email->Host = 'sandbox.smtp.mailtrap.io';
+        $email->Host = $_ENV['EMAIL_HOST'];
         $email->SMTPAuth = true;
-        $email->Port = 2525;
-        $email->Username = '';
-        $email->Password = '';
+        $email->Port = $_ENV['EMAIL_PORT'];
+        $email->Username = $_ENV['EMAIL_USER'];
+        $email->Password = $_ENV['EMAIL_PASSWORD'];
 
         $email->setFrom('salonAccount@test.com');
         $email->addAddress('salonAccount@tests.com','salon.com');
@@ -65,7 +65,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 
         $content = "<html>";
         $content .= "<p> Hi: ".$this->name." Reset password by click following link</p>";
-        $content .= "<a href='http://localhost:8080/recover?token=".$this->token."'>Reset Password</a>";
+        $content .= "<a href='".$_ENV['APP_URL']."/recover?token=".$this->token."'>Reset Password</a>";
         $content .= "<p>If you did not create a new account, please ignore the email.</p>";
         $content .= "</html>";
         $email->Body = $content;
